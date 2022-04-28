366aa0f9c8a373d5856ae8e84e8e89b6baaaf0e9
sheilamalik00 <sheilazahramalik@gmail.com>
2022-04-28 13:20:42 +0700

    frontend

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -32,45 +32,9 @@
     <!-- <div class="preloader"></div> -->
     <!-- Preloader -->
 
-    <!--Header Upper-->
-    <section class="header-uper">
-      <div class="container clearfix">
-        <div class="logo">
-          <figure>
-            <a href="index.html">
-              <img src="images/RSIAKB.png" alt="" width="230">
-            </a>
-          </figure>
-        </div>
-        <div class="right-side">
-          <ul class="contact-info">
-            <li class="item">
-              <div class="icon-box">
-                <i class="fa fa-envelope-o"></i>
-              </div>
-              <strong>E-mail</strong>
-              <br>
-              <a href="#">
-                <span>rsiakaruniabunda@yahoo.com</span>
-              </a>
-            </li>
-            <li class="item">
-              <div class="icon-box">
-                <i class="fa fa-phone"></i>
-              </div>
-              <strong>Telepon</strong>
-              <br>
-              <span>021-5520356</span>
-            </li>
-          </ul>
-          <div class="link-btn">
-            <a href="#" class="btn-style-one">Buat Janji Temu</a>
-          </div>
-        </div>
-      </div>
-    </section>
-    <!--Header Upper-->
-
+    
+           
+@include(frontend.tools.header)
 
     <!--Main Header-->
     <nav class="navbar navbar-default">
