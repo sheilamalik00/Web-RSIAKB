5bf154f4076cec311197dbc64ce48731a2b4a4b5
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-06-03 10:55:19 +0700

    fixing menu service schedule

--- app/Http/Controllers/Frontend/Schedule/ScheduleController.php
+++ app/Http/Controllers/Frontend/Schedule/ScheduleController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers\Frontend\Schedule;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Doctor;
+use App\Models\SpecialistDoctor;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index()
+    {
+        $dokter = Doctor::with('get_specialist_doctor')->get();
+        $blog = Blog::latest()->limit(2)->get();
+        $doctor = Doctor::latest()->limit(3)->get();
+        // $doctors = Doctor::all();
+        $specialist = SpecialistDoctor::all();
+        return view('frontend.schedule.index',compact('blog','doctor','dokter','specialist'));
+        // return view('frontend.schedule.index', compact('dokter'));
+        // return view('frontend.schedule.index');
+    }
 }
---
+++ resources/views/frontend/schedule/index.blade.php
@@ -0,0 +1,125 @@
+@extends('frontend.master')
+
+@section('content')
+  <!--Page Title-->
+  <section class="page-title text-center"
+    style="background-image:url({{ asset('asset/frontend/images/background/3.jpg') }});">
+    <div class="container">
+      <div class="title-text">
+        <h1>Buat Janji Temu</h1>
+        <ul class="title-menu clearfix">
+          <li>
+            <a href="index.html">Beranda &nbsp;/</a>
+          </li>
+          <li>Buat Janji Temu</li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <!--End Page Title-->
+
+  <!-- Contact Section -->
+  <section class="blog-section section style-three pb-0">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <div class="contact-area">
+            <div class="section-title">
+              <h3>Request
+                <span>Janji Temu</span>
+              </h3>
+            </div>
+            <form name="contact_form" class="default-form contact-form" action="{{ route('kirim-jadwal') }}" method="get">
+              <div class="row">
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                  <div class="form-group">
+                    <input type="text" name="name" placeholder="Name" required="">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" name="noktp" placeholder="Nomor KTP" required="">
+                  </div>
+
+                  <div class="form-group">
+                    <select name="poli" id="spesialis">
+                      <option>Pilih Spesialis</option>
+
+                      @foreach ($specialist as $index)
+                        <option value="{{ $index->id }}">{{ $index->name }}</option>
+                      @endforeach
+                    </select>
+                  </div>
+                </div>
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                  <div class="form-group">
+                    <input type="text" name="nophone" placeholder="No Telepon" required="">
+                  </div>
+                  <div class="form-group">
+                    <input type="email" name="email" placeholder="email">
+                  </div>
+
+                  <div class="form-group">
+                    <input type="text" name="tgl_lahir" placeholder="Tanggal Lahir" required="" id="datepicker">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                  </div>
+                  <div class="form-group">
+                    <input type="date" id="tgl_berobat" class="tgl_berobat" name="tgl_berobat"
+                      placeholder="Tanggal Berobat" required="">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                  </div>
+                </div>
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                  <div class="form-group">
+                    <select name="doctor" id="doctor">
+                      <option>Dokter</option>
+
+                    </select>
+                  </div>
+                  <div class="form-group text-center">
+                    <button type="submit" class="btn-style-one">Kirim</button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12">
+          <div class="appointment-image-holder">
+            <figure>
+              <img src="{{ asset('asset/frontend/images/background/appoinment.jpg') }}" alt="Appointment">
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!--team section-->
+  <section class="team-section section">
+    <div class="container">
+      <div class="section-title text-center">
+        <h3>Dokter Terbaik
+          <span>Kami</span>
+        </h3>
+        {{-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem illo, rerum
+          <br>natus nobis deleniti doloremque minima odit voluptatibus ipsam animi?
+        </p> --}}
+      </div>
+      <div class="row">
+        @foreach ($dokter as $item)
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="team-member">
+              <img src="{{$item->get_image()}}" alt="doctor" class="img-responsive">
+              <div class="contents text-center">
+                <h4>{{$item->name}}</h4>
+                <p>{{$item->get_specialist_doctor->name}}</p>
+                {{-- <a href="#" class="btn btn-main">read more</a> --}}
+              </div>
+            </div>
+          </div>
+        @endforeach
+      </div>
+    </div>
+  </section>
+  <!--End team section-->
+@endsection
