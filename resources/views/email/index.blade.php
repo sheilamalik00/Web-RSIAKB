be8efdd8029812da8af0a2b17ca18731af56d70a
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-08-22 12:30:18 +0700

    fixing email

--- resources/views/email/index.blade.php
+++ resources/views/email/index.blade.php
@@ -4,12 +4,12 @@
 </head>
 <body>
     <h3>No Antrian Anda: {{ $data['no'] }}</h3>
-    <h4>{{ $data['body'] }}</h4><br/>
-    <h6>Nama Pasien: {{ $data['name'] }}</h6>
-    <h6>No HP: {{ $data['phone'] }}</h6>
-    <h6>Tanggal: {{ $data['birth'] }}</h6>
-    <h6>Tnggal Periksa: {{ $data['treatment_date'] }}</h6>
-    <h6>Dokter : {{ $data['doctor_name'] }}</h6>
+    <p>{{ $data['body'] }}</p><br/>
+    <p>Nama Pasien: {{ $data['name'] }}</p>
+    <p>No HP: {{ $data['phone'] }}</p>
+    <p>Tanggal Lahir: {{ $data['birth'] }}</p>
+    <p>Tanggal Periksa: {{ $data['treatment_date'] }}</p>
+    <p>Dokter Tujuan : {{ $data['doctor_name'] }}</p>
     <p>Terima Kasih Telah Menggunakan Layanan Kami</p>
 </body>
 </html>
